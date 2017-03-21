0720cf6bdfcc2fe1921d67ab6e856f6f6d50fccd
yi0x262 <yutadefn@gmail.com>
2017-03-21 16:47:53 +0900

    remove const

--- ADCPiPlus.hpp
+++ ADCPiPlus.hpp
@@ -2,8 +2,8 @@
 
 class ADCPiPlus : RPi_i2c
 {
-  char const adctx[] = {0x01,0x80,0x00};
-  char const adcrx[3];
+  char adctx[] = {0x01,0x80,0x00};
+  char adcrx[3];
   uint32_t spi_speed = static_cast<int>(2.5e6);
   double adcrefvoltage = 3.3;
   void modeset(const int channel,const int mode);
