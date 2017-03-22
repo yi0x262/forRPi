13937121f3cfe09464e6ebef3ff257185a29e3f4
yi0x262 <yutadefn@gmail.com>
2017-03-22 17:13:57 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -6,17 +6,16 @@
 
 int main(int argc,char* argv[])
 {
-  std::vector<ADCPiPlus> adc;
-
   int address = atoi(argv[1]);
   int conversion = atoi(argv[2]);
   int bit_rate = atoi(argv[3]);
 
+  ADCPiPlus adc(address,conversion,bit_rate,1);
+
   std::cout << address << conversion << bit_rate << std::endl;
-  for(auto c:{1,2,3,4})adc.push_back(ADCPiPlus(address,c,conversion,bit_rate,1));
   while(1)
   {
-    for(auto& a : adc)std::cout << a.read();
+    for(auto c : {1,2,3,4})std::cout << adc.read(c) << "\t";
     std::cout << std::endl;
   }
 
