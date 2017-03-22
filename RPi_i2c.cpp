8ac68c6189c57d5bf5f7db515eaf54da535b144a
yi0x262 <yutadefn@gmail.com>
2017-03-22 16:07:28 +0900

    add block

--- RPi_i2c.cpp
+++ RPi_i2c.cpp
@@ -12,16 +12,20 @@ using std::runtime_error;
 RPi_i2c::RPi_i2c(const char device[], const int address) : RPi_io(device)
 {
   if(ioctl(fd,I2C_SLAVE,address)<0)
+  {
     printf("fd=%d\terrno=:%d,\t%s\n",fd,errno,strerror(errno));
     throw runtime_error("i2c address set error : unable access");
+  }
 }
 
 int RPi_i2c::_write(char buffer[]) const
 {
   int ret;
   if((ret = write(fd,buffer,strlen(buffer))) < 0)
+  {
     printf("fd=%d\terrno=:%d,\t%s\n",fd,errno,strerror(errno));
     throw runtime_error("i2c write error");
+  }
   return ret;
 }
 
@@ -29,7 +33,9 @@ int RPi_i2c::_read(char buffer[]) const
 {
   int ret;
   if((ret = read(fd,buffer,sizeof(buffer))) < 0)
+  {
     printf("fd=%d\terrno=:%d,\t%s\n",fd,errno,strerror(errno));
     throw runtime_error("i2c read error");
+  }
   else return ret;
 }
