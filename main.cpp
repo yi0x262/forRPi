409128af1d67d7e163ea4a6ac94151eb5e0cb47f
yi0x262 <yutadefn@gmail.com>
2017-03-22 15:40:55 +0900

    1~4

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 
 int main(int argc,char* argv[])
 {
-  int address = atoi(argv[1]);
-  int channel = atoi(argv[2]);
-  int conversion = atoi(argv[3]);
-  int bit_rate = atoi(argv[4]);
+  ADCPiPlus adc[4];
 
-  std::cout << address << channel << conversion << bit_rate << std::endl;
-  ADCPiPlus adc(address,channel,conversion,bit_rate,1);
-  while(1)std::cout << adc.read() << std::endl;
+  int address = atoi(argv[1]);
+  //int channel = atoi(argv[2]);
+  int conversion = atoi(argv[2]);
+  int bit_rate = atoi(argv[3]);
+
+  std::cout << address << conversion << bit_rate << std::endl;
+  for(int c = 0; c < 4; ++c)adc[c] = ADCPiPlus(address,c+1,conversion,bit_rate,1);
+  while(1)
+  {
+    for(auto& a : adc)std::cout << a.read()
+    std::cout << std::endl;
+  }
 
   return 0;
 }
