0a2744ed685ef699984c2b2b04078c84366e8cfc
yi0x262 <yutadefn@gmail.com>
2017-03-22 15:57:05 +0900

    vector

--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 
 #include<iostream>
 #include<cstdlib>
+#include<vector>
 
 int main(int argc,char* argv[])
 {
-  ADCPiPlus* adc = new ADCPiPlus[4];
+  std::vector<ADCPiPlus> adc;
 
   int address = atoi(argv[1]);
-  //int channel = atoi(argv[2]);
   int conversion = atoi(argv[2]);
   int bit_rate = atoi(argv[3]);
 
   std::cout << address << conversion << bit_rate << std::endl;
-  for(int c = 0; c < 4; ++c)adc[c] = ADCPiPlus(address,c+1,conversion,bit_rate,1);
+  for(auto c:{1,2,3,4})adc.push_back(ADCPiPlus(address,c,conversion,bit_rate,1));
   while(1)
   {
     for(auto& a : adc)std::cout << a.read();
