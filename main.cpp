545ba955f257cfe42ba501b51cc3312a784d006b
yi0x262 <yutadefn@gmail.com>
2017-03-22 14:59:44 +0900

    pga = 1

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@ int main(int argc,char* argv[])
   int channel = atoi(argv[2]);
   int conversion = atoi(argv[3]);
   int bit_rate = atoi(argv[4]);
-  int pga = atoi(argv[5]);
 
-  std::cout << address << channel << conversion << bit_rate << pga << std::endl;
-  ADCPiPlus adc(address,channel,conversion,bit_rate,pga);
+  std::cout << address << channel << conversion << bit_rate << std::endl;
+  ADCPiPlus adc(address,channel,conversion,bit_rate,1);
   while(1)std::cout << adc.read() << std::endl;
 
   return 0;
