52ec5af7236ec7402b997fbf7b735e890185a373
yi0x262 <yutadefn@gmail.com>
2017-03-23 11:47:18 +0900

    print adcrx

--- ADCPiPlus.cpp
+++ ADCPiPlus.cpp
@@ -42,12 +42,14 @@ double ADCPiPlus::read(const int channel)
   if(adctx&0x0c == 0x0c)//bitrate:18
   {
     _read(adcrx);
+    std::cout << adcrx << std::endl;
     raw = ((adcrx[0]&0x03)<<16) | (adcrx[1] << 8) | adcrx[2];
     raw &= (raw>>17) ? 0:~(1<<17);
   }
   else
   {
     _read(&adcrx[1]);
+    std::cout << adcrx << std::endl;
     switch(adctx&0x0c >> 2)
     {
       case 0://bitrate:12
