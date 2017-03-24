58bc9f73ce96c079815709e25fda7521ad1379a0
yi0x262 <yutadefn@gmail.com>
2017-03-24 10:00:52 +0900

    add sendbuf

--- L3DG20.cpp
+++ L3DG20.cpp
@@ -7,7 +7,8 @@ int L3DG20::read_xyz(const int reg)
 {
   for(auto i:{0,1})
   {
-    _write(static_cast<char>(reg+i));
+    char send = static_cast<char>(reg+i);
+    _write(&send);
     _read(&buf.buf[!i]);//x_low:0x28,x_high:0x29
   }
   return static_cast<int>(buf.num);
