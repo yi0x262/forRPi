b477a47374880f0df771d4faec49a95f0c10c7be
yi0x262 <yutadefn@gmail.com>
2017-03-24 09:54:04 +0900

    make main

---
+++ main_L3DG20.cpp
@@ -0,0 +1,19 @@
+#include"L3DG20.hpp"
+
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>//usleep
+
+int main(int argc,char* argv[])
+{
+  L3DG20 l;
+  while(1)
+  {
+    std::cout << l.read_x() << "\t";
+    std::cout << l.read_y() << "\t";
+    std::cout << l.read_z() << "\n";
+
+    usleep(atoi(argv[1]));
+  }
+  return 0;
+}
