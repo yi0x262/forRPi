384b28bf40ad67a49cbc222586a3f3da1458d5df
yi0x262 <yutadefn@gmail.com>
2017-03-29 14:18:09 +0900

    display send[0]

--- L3DG20.cpp
+++ L3DG20.cpp
@@ -34,6 +34,7 @@ int L3DG20::read_xyz(const int reg)
   for(auto i:{0,1})
   {
     send[0] = static_cast<char>(reg+i);
+    std::cout <<std::bitset<8>(send[0]) << " ";// << std::bitset<8>(receive[0])<<std::bitset<8>(receive[1]) << " ";
     _write(send);
     _read(send);//x_low:0x28,x_high:0x29
     std::cout <<std::bitset<8>(send[0]) << " ";// << std::bitset<8>(receive[0])<<std::bitset<8>(receive[1]) << " ";
