7e882c0864db0f2e69522b19de5b7ee71be18b34
yi0x262 <yutadefn@gmail.com>
2017-03-29 14:19:40 +0900

    more wait

--- L3DG20.cpp
+++ L3DG20.cpp
@@ -39,7 +39,7 @@ int L3DG20::read_xyz(const int reg)
     _read(send);//x_low:0x28,x_high:0x29
     std::cout <<std::bitset<8>(send[0]) << " ";// << std::bitset<8>(receive[0])<<std::bitset<8>(receive[1]) << " ";
     receive[i] = send[0];
-    usleep(10000);
+    usleep(1000000);
   }
   //std::cout <<std::bitset<8>(send[0]) << " " << std::bitset<8>(receive[0])<<std::bitset<8>(receive[1]) << " ";
   return static_cast<int>(buf.num);
