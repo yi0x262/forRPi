0492fd0d5dcac9bc1c8c3c4bd1376fb0b0772f4b
yi0x262 <yutadefn@gmail.com>
2017-04-29 23:04:27 +0900

    dev -> 1

--- main_i2c.cpp
+++ main_i2c.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc,char* argv[])
 {
-  i2c_file adc1("/dev/i2c-dev",0x68);
-  i2c_file adc2("/dev/i2c-dev",0x69);
-  i2c_file gyro("/dev/i2c-dev",0x6b);
+  i2c_file adc1("/dev/i2c-1",0x68);
+  i2c_file adc2("/dev/i2c-1",0x69);
+  i2c_file gyro("/dev/i2c-1",0x6b);
   return 0;
 }
