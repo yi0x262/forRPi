3963dfe729276ce94ee0b47efc5cc1b2031aca65
yi0x262 <yutadefn@gmail.com>
2017-04-29 22:31:25 +0900

    instance test

--- main_i2c.cpp
+++ main_i2c.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc,char* argv[])
 {
-  i2c_descriptor fd(argv[1],0x6a);
+  i2c_descriptor adc1(argv[1],0x68);
+  i2c_descriptor adc2(argv[1],0x69);
+  i2c_descriptor gyro(argv[1],0x6b);
   return 0;
 }
