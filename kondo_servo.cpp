bc5a92847fb798d8738bb2ee50b1851299fb9308
yi0x262 <yutadefn@gmail.com>
2017-05-02 12:12:44 +0900

    write

---
+++ kondo_servo.cpp
@@ -0,0 +1,31 @@
+#include"kondo_servo.hpp"
+
+kondo_servo::kondo_servo(const char device[], const int servo_id):
+  fd(file(device)),id(servo_id){}
+
+unsigned short kondo_servo::rotate(unsigned short target_angle)
+{
+  char buf[6];
+  fd._write({(char)0x80+id,(char)(target_angle>>7)&0x7f,(char)target_angle&0x7f});
+  fd._read(buf,6);
+  return static_cast<unsigned short>(buf[4])<<7 + static_cast<unsigned short>(buf[5]);
+}
+
+char kondo_servo::current(void){
+  char buf[5];
+  fd._write({(char)0xa0+id,(char)0x03});
+  fd._read(buf,5);
+  return buf[4];
+}
+
+char kondo_servo::temperature(void){
+  char buf[5];
+  fd._write({(char)0xa0+id,(char)0x04});
+  fd._read(buf,5);
+  return buf[4];
+}
+
+kondo_servo::~kondo_servo()
+{
+  fd.~file();
+}
---
+++ kondo_servo.hpp
@@ -0,0 +1,15 @@
+#include"file_descriptor.hpp"
+
+class kondo_servo:
+{
+  const int id;
+  file fd;
+public:
+  kondo_servo(const char device[],const int servo_id);
+
+  unsigned short rotate(unsigned short target_angle);//2byte
+  char current(void);//1byte
+  char temperature(void);//1byte
+
+  ~kondo_servo();
+}
