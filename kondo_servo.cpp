84ab541aad91995508ce531065238537e93b7c47
yi0x262 <yutadefn@gmail.com>
2017-05-02 13:30:55 +0900

    add ()

--- kondo_servo.cpp
+++ kondo_servo.cpp
@@ -5,7 +5,7 @@ kondo_servo::kondo_servo(const int servo_id):
 
 unsigned short kondo_servo::rotate(unsigned short target_angle)
 {
-  char wbuf[3] = {(char)0x80+id,(char)(target_angle>>7)&0x7f,(char)target_angle&0x7f};
+  char wbuf[3] = {(char)(0x80+id),(char)((target_angle>>7)&0x7f),(char)(target_angle&0x7f)};
   fd._write(wbuf);
   char rbuf[6];
   fd._read(rbuf,6);
@@ -13,7 +13,7 @@ unsigned short kondo_servo::rotate(unsigned short target_angle)
 }
 
 char kondo_servo::current(void){
-  char wbuf = {(char)0xa0+id,(char)0x03};
+  char wbuf = {(char)(0xa0+id),(char)0x03};
   fd._write(wbuf);
   char buf[5];
   fd._read(buf,5);
@@ -21,7 +21,7 @@ char kondo_servo::current(void){
 }
 
 char kondo_servo::temperature(void){
-  char wbuf = {(char)0xa0+id,(char)0x04};
+  char wbuf = {(char)(0xa0+id),(char)0x04};
   fd._write(wbuf);
   fd._read(buf,5);
   return buf[4];
