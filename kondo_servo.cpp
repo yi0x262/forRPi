7c968d6d854788a44aed003489f3eaa97414118a
yi0x262 <yutadefn@gmail.com>
2017-05-02 13:29:21 +0900

    brace-enclosed initializer list

--- kondo_servo.cpp
+++ kondo_servo.cpp
@@ -5,22 +5,24 @@ kondo_servo::kondo_servo(const int servo_id):
 
 unsigned short kondo_servo::rotate(unsigned short target_angle)
 {
-  char buf[6];
-  fd._write({(char)0x80+id,(char)(target_angle>>7)&0x7f,(char)target_angle&0x7f});
-  fd._read(buf,6);
-  return static_cast<unsigned short>(buf[4])<<7 + static_cast<unsigned short>(buf[5]);
+  char wbuf[3] = {(char)0x80+id,(char)(target_angle>>7)&0x7f,(char)target_angle&0x7f};
+  fd._write(wbuf);
+  char rbuf[6];
+  fd._read(rbuf,6);
+  return static_cast<unsigned short>(rbuf[4])<<7 + static_cast<unsigned short>(rbuf[5]);
 }
 
 char kondo_servo::current(void){
+  char wbuf = {(char)0xa0+id,(char)0x03};
+  fd._write(wbuf);
   char buf[5];
-  fd._write({(char)0xa0+id,(char)0x03});
   fd._read(buf,5);
   return buf[4];
 }
 
 char kondo_servo::temperature(void){
-  char buf[5];
-  fd._write({(char)0xa0+id,(char)0x04});
+  char wbuf = {(char)0xa0+id,(char)0x04};
+  fd._write(wbuf);
   fd._read(buf,5);
   return buf[4];
 }
