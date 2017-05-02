d999dd374d2fe3a52e8d74ba04f7e8ac4022b1c3
yi0x262 <yutadefn@gmail.com>
2017-05-02 13:06:12 +0900

    write

---
+++ main_kondo.cpp
@@ -0,0 +1,40 @@
+#include"kondo_servo.hpp"
+#include"tcp_client.hpp"
+
+union us_c
+{
+  unsigned short i;
+  char c[2];
+}
+
+#include<vector>
+using std::vector;
+#include<algorithm>
+
+int main(int argc, char* argv[])
+{
+  tcp_client tc(argv[1],12345);
+
+  kondo_servo ks[];
+  for(auto id : {0,1,2,3,4})
+  {
+    ks[i] = kondo_servo("/dev/ttyAMA0",id);
+  }
+
+  vector<char> buf;
+  char *cbuf = new char[buf.size()];
+
+  while(true)
+  {
+    for(auto k : ks)
+    {
+      us_c angle.i = k.rotate(7500);
+      buf.push_back(angle.c[0]);
+      buf.push_back(angle.c[1]);
+      buf.push_back(k.current());
+      buf.push_back(k.temperature());
+    }
+    std::copy(buf.begin(),buf.end(),cbuf);
+    tc.transceive(cbuf);
+  }
+}
