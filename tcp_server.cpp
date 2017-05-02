9cba02f03cc077aaef79ab1568e959b137026eae
yi0x262 <yutadefn@gmail.com>
2017-05-02 13:56:12 +0900

    on the way

---
+++ tcp_server.cpp
@@ -0,0 +1,10 @@
+#include"tcp_server.hpp"
+
+#include<boost/asio.hpp>
+using ip = boost::asio::ip
+
+tcp_server::tcp_server(const char server_name[], const u_short port_num)
+  :tcp_base(),
+{
+
+}
--- tcp_server.hpp
+++ tcp_server.hpp
@@ -1,1 +1,9 @@
 #include"tcp_base.hpp"
+
+class tcp_server:tcp_base
+{
+  boost::asio::ip::tcp::acceptor acc;
+public:
+  tpc_server(const char server_name[],const u_short port_num);
+  int transceive(char msg[]);
+};
