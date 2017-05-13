928b616764d4906d8ce703bc3021a26fdc081815
quhp <humanoid.9dai@gmail.com>
2017-05-13 14:31:33 +0900

    makefile

--- main_kondo.cpp
+++ main_kondo.cpp
@@ -1,37 +1,13 @@
 #include"kondo_servo.hpp"
-#include"tcp_client.hpp"
 
-union us_c
-{
-  unsigned short i;
-  char c[2];
-};
-
+#include<cstdlib>
 #include<vector>
 using std::vector;
-#include<algorithm>
 
 int main(int argc, char* argv[])
 {
-  tcp_client tc(argv[1],12345);
+  vector<kondo_servo> ks;
+  for(int i = 1; i < argc; ++i)ks.push_back(kondo_servo(atoi(argv[i])));
 
-  kondo_servo ks[5]{0,1,2,3,4};
-
-  vector<char> buf;
-  char *cbuf = new char[buf.size()];
-
-  while(true)
-  {
-    for(auto k : ks)
-    {
-      us_c angle;
-      angle.i = k.rotate(7500);
-      buf.push_back(angle.c[0]);
-      buf.push_back(angle.c[1]);
-      buf.push_back(k.current());
-      buf.push_back(k.temperature());
-    }
-    std::copy(buf.begin(),buf.end(),cbuf);
-    tc.transceive(cbuf);
-  }
+  return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+servo_local: main_kondo.o kondo_servo.o file_descriptor.o
+	g++ -o servo_local main_kondo.o kondo_servo.o file_descriptor.o
