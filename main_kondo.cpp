5b9a8c1a81120741e25f7b5fd5b04833d45f6ed6
quhp <humanoid.9dai@gmail.com>
2017-05-13 14:43:05 +0900

    print current&temperature

--- main_kondo.cpp
+++ main_kondo.cpp
@@ -4,10 +4,17 @@
 #include<vector>
 using std::vector;
 
+#include<iostream>
+
 int main(int argc, char* argv[])
 {
   vector<kondo_servo> ks;
   for(int i = 1; i < argc; ++i)ks.push_back(kondo_servo(atoi(argv[i])));
+
+  for(auto k:ks)
+  {
+    std::cout << static_cast<int>(k.current()) << " " << static_cast<int>(k.temperature()) << std::endl;
+  }
 
   return 0;
 }
