da89cedc514077236be86f1620b5c6231237315e
quhp <humanoid.9dai@gmail.com>
2017-05-13 15:44:36 +0900

    1

--- main_kondo.cpp
+++ main_kondo.cpp
@@ -10,14 +10,10 @@ using std::vector;
 
 int main(int argc, char* argv[])
 {
-  vector<kondo_servo> ks;
-  for(int i = 1; i < argc; ++i)ks.push_back(kondo_servo(atoi(argv[i])));
+  kondo_servo k(22);
 
-  for(auto k:ks)
-  {
     std::cout << static_cast<int>(k.current()) << std::endl;
     std::cout << static_cast<int>(k.temperature()) << std::endl;
-  }
 
   return 0;
 }
