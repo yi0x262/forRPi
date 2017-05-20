81a8dd2d065b74603ae2ff1a80f51663334df4a8
quhp <humanoid.9dai@gmail.com>
2017-05-20 17:23:07 +0900

    remove const

--- main_tcp_client.cpp
+++ main_tcp_client.cpp
@@ -16,21 +16,23 @@ union sc
 };
 
 int main(int argc,const char* argv[]){
-  tcp_client tc(argv[1],12345);
+  //tcp_client tc(argv[1],12345);
 
-  vector<kondo_servo> ks;
-  for(int i : {0,1,6,7})ks.push_back(kondo_servo(i));
+  //vector<kondo_servo> ks;
+  kondo_servo* ks[4];
+  int n = 0;
+  for(int i : {0,1,6,7})ks[n++] = new kondo_servo(i);
 
   while(true){
     string str;
     for(auto k : ks)
     {
       sc r;
-      r.s = k.rotate(7500);
+      r.s = k.rotate(11500);
       str += r.c;
+      cout << r.s << endl;
     }
-    cout << str << endl;
-    tc.write(str.c_str());
+    //tc.write(str.c_str());
   }
 
   return 0;
