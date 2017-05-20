d3617c1ca51f2f122b91abad91794284ba4b8fed
quhp <humanoid.9dai@gmail.com>
2017-05-20 14:39:26 +0900

    recv ip as arg

--- main_tcp_client.cpp
+++ main_tcp_client.cpp
@@ -4,11 +4,12 @@
 #include<vector>
 using std::vector;
 
-int main(){
-  tcp_client tc("192.168.1.8",12345);
+int main(int argc,const char* argv[]){
+  tcp_client tc(argv[1],12345);
 
   vector<kondo_servo> ks;
   for(int i : {0,1,6,7})ks.push_back(kondo_servo(i));
 
   return 0;
+
 }
